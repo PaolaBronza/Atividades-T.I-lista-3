63395b10a16a5513074dcdc4992b53d6a415a0da
PaolaBronza <125701556+PaolaBronza@users.noreply.github.com>
2023-02-23 09:22:28 -0300

    Rename Atividade3-2.cpp to Atividade3-2.c

--- Atividade3-2.c
+++ Atividade3-2.c
@@ -10,14 +10,14 @@ int main(void){
 	scanf("%f",&num);
 	
 	if(num<18){
-		printf("você ainda é menor de idade");
+		printf("vocÃª ainda Ã© menor de idade");
 	num2=18-num;
 		printf("\nfaltam %d anos para atingir a maioridade",num2);
 	}
 	else if(num>=18){
-		printf("você já é maior de idade");
+		printf("vocÃª jÃ¡ Ã© maior de idade");
 	num3=num-18;
-		printf("\natingiu a maioridade há %d anos",num3);
+		printf("\natingiu a maioridade hÃ¡ %d anos",num3);
 	}
 	printf("\nmaioridade: 18 anos");
 	return 0;
